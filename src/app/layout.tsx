3c81fb24b2b8ed8dcce3cde08a151d2809017e50
Guilherme Oliveira <g.alves.oliveira@outlook.com>
2025-04-03 12:04:42 -0300

    add icons lib, zod integration with forms, tailwind prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="flex h-screen flex-col bg-white">
       <body>
         <Providers>{children}</Providers>
       </body>
